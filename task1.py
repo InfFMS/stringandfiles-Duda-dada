526f9c1aad2762e764b82c1b9949ec93300297a4
Дуда Ирина Дмитриевна <i.duda@main.nsu.ru>
2025-01-20 14:25:21 +0700

    k

--- task1.py
+++ task1.py
@@ -5,7 +5,16 @@
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
 
+import string
 
+
+with open("task1.txt", "r", encoding="utf-8") as f:
+    file_line = f.read()
+print(file_line)
+a_nums=0
+for char in file_line:
+    if char == "—":
+        a_nums+=1
 with open("task1.txt", "r", encoding="utf-8") as f:
     file_line = f.readlines()
 line_count = len(file_line)
@@ -18,5 +27,5 @@ for line in file_line:
     symbol_count += len(line)
 
 print("Общее количество строк:", line_count)
-print("Общее количество слов:", word_count)
+print("Общее количество слов:", word_count- a_nums)
 print("Общее количество символов (включая пробелы):", symbol_count)
